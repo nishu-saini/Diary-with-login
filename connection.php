f6837f8572c29a5ca42f4ed0208a150fddfb43eb
nishu-saini <57725190+nishu-saini@users.noreply.github.com>
2021-01-12 17:06:15 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    #conection to mysql database...
+    #conect to mysql database...
     $link = mysqli_connect("localhost", "quarn", "Chra-XXZP3eui$&", "secret_diary");
             if(mysqli_connect_error()) {
                 die ("Database connection error!");
